a0308dbfc1ed3de7486d807a458184d0e7fa76b3
Seth Armistead <sdarmistead@gmail.com>
2025-11-13 15:27:46 +0100

    added comments in the code

--- scripts/aboutUs.js
+++ scripts/aboutUs.js
@@ -12,25 +12,22 @@ function teamMate(first, last, country, description, pet) {
 let callum = new teamMate('Callum', 'Jones', 'Callum is from Australia', 'Callum has been fascinated by animals all his life. Having been raised by kangaroos, he knows what it is like to truly be part of mother nature. Callum was originally featured in an exhibit, but after several court cases we included him on our staff. You will find him hopping between exhibits.', 'Callum has a pet crocodile whom he named "Rob" - after his favorite teacher.')
 let seth = new teamMate('Seth', 'Armistead', 'Seth is from USA', 'Seth has worked for Quokka zoo for three years. Since then, he has "become one" with the tasmanian devils. Being one of our primary tour guides here at the zoo, Seth introduces visitors to the idea of "true environmentalism", and what it means to get in the mind of some of our most interesting species.', "Seth doesn't have any pets.")
 let clara = new teamMate('Clara', 'Swann', 'Clara is from Sweden.', 'Clara is a volunteer here at Quokka zoo. After repeatedly climbing over fences late at night to visit the quokka exhibit, the personell decided to allow Clara access. She is almost always inside the quokka exhibit, obsessing over their cuteness.', 'Clara has two pet guinea pigs.')
-let brianna = new teamMate('Brianna', 'Swann', 'Brianna is from China', 'Brianna is our zoos bird enthusiast. She can make over 300 bird sounds, and some people even think she can communicate with the birds at the zoo. Brianna spends most of her time scrambling up trees in pursuit of kookaburras, who generally avoid her. ', 'Brianna has a cat.')
+let brianna = new teamMate('Brianna', 'Fraser', 'Brianna is from China', 'Brianna is our zoos bird enthusiast. She can make over 300 bird sounds, and some people even think she can communicate with the birds at the zoo. Brianna spends most of her time scrambling up trees in pursuit of kookaburras, who generally avoid her. ', 'Brianna has a cat.')
 let nathalie = new teamMate('Nathalie', 'Buss', 'Nathalie is from Sweden', 'Nathalie became a part of Quokka zoo after falling into the Perentie exhibit and being too afraid to climb out. You will find her desperately avoiding the Perentie, who have become indifferent to her presence. Since she has been here for so long, we decided to make her part of the team.', 'Nathalie has a cat.')
 let defaultMessage = new teamMate('Team', 'Members', '', 'Our team has members from all over the world! We work together to maintain a healthy and safe environment for all of the animals that we are responsible for. Our goal is to present the most interesting species in Australia in an engaging and authentic way to everyone who steps through our gate.', '')
 
 let teamMates = [callum, seth, clara, brianna, nathalie]
 
 ////Functions////
-const setActiveMember = (member) => activeTeamMate = member
 
-const appendContainer = (member) => {
-  let textArea = document.querySelector('.main_content')
-  let subtitle = document.querySelector('.subtitle')
-  subtitle.textContent = `${member.first} ${member.last}`
-}
-
+///adds each key-value pair with the key as the classname and the value as the text content, also adds title///
 const appendMember = (member) => {
   let memberArea = document.querySelector('.team_members')
+  //resets the content//
   memberArea.innerHTML = ''
-  appendContainer(member)
+  let subtitle = document.querySelector('.subtitle')
+  subtitle.textContent = `${member.first} ${member.last}`
+  //converts to an array, and skips the first and last name [0 and 1]//
   let memberPairs = Object.entries(member)
   for (let i = 2; i < memberPairs.length; i++) {
     let memberData = document.createElement('div')
@@ -41,21 +38,25 @@ const appendMember = (member) => {
   listTeamMates(member)
 }
 
+///Adds team members to the sidebar///
 const listTeamMates = (active = defaultMessage) => {
   let sidebar = document.querySelector('.sidebar')
+  //resets the content//
   sidebar.innerHTML = ''
   teamMates.forEach(member => {
     let teamMate = document.createElement('div')
     teamMate.textContent = member.first
     teamMate.className = 'sidebarItem'
+    //Adds active class and event listener to go to default message if clicked again//
     if (member === active){
-    teamMate.addEventListener('click', () => {setActiveMember(member), appendMember(defaultMessage)}, {once:true})
+    teamMate.addEventListener('click', () => {appendMember(defaultMessage)}, {once:true})
     teamMate.className = 'sidebarItem active'
     } else {
-    teamMate.addEventListener('click', () => {setActiveMember(member), appendMember(member)}, {once:true})
+    teamMate.addEventListener('click', () => {appendMember(member)}, {once:true})
     }
     sidebar.appendChild(teamMate)
   });
 }
 
+//initializes the default message//
 appendMember(defaultMessage)
