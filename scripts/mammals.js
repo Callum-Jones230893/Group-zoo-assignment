dde62872fd0cff2394dc653b60c21520dc1ad281
Nathalie <nathaliebuss21@gmail.com>
2025-11-18 16:06:22 +0100

    adjusting js for add listeners

--- scripts/mammals.js
+++ scripts/mammals.js
@@ -10,38 +10,67 @@ function Mammals(name, lifespan, group, food, description, length, weight, found
 }
 
 let echidna = new Mammals ("Echidna", "50 years", "mammals", "insects such as ants and termites, beetle larvae and worms", "Echidnas, also called spiny anteaters, are walking contradictions. They are mammals, but they lay eggs. They are often classified as long- or sort-beaked, but don't have beaks at all, in the traditional sense; they have fleshy noses that can be either on the long side or rather short. They don't really look like true anteaters (Myrmecophaga tridactyla), either, and they are not closely related to them. They are spiny, though; their bodies are covered with hollow, barbless quills. Echidnas are monotremes, egg-laying mammals. The only other living monotreme is the platypus.", "76 cm", "10kg", "Throughout Australia.")
-let tasmanianDevil = new Mammals ("Tasmanian Devil", "5 years", "mammals", "A predator, then eat meat from other animals such as wallabies and wombats", "The Tasmanian devil (Sarcophilus harrisii) (palawa kani: purinina)[3] is a carnivorous marsupial of the family Dasyuridae. Until recently, it was only found on the island state of Tasmania, but it has been reintroduced to New South Wales in mainland Australia, with a small breeding population. The size of a small dog, the Tasmanian devil became the largest carnivorous marsupial in the world, following the extinction of the thylacine in 1936. It is related to quolls, and distantly related to the thylacine. It is characterised by its stocky and muscular build, black fur, pungent odour, extremely loud and disturbing screech, keen sense of smell, and ferocity when feeding. The Tasmanian devil's large head and neck allow it to generate among the strongest bites per unit body mass of any extant predatory land mammal. It hunts prey and scavenges on carrion.", "70 cm", "10kg", "Tasmania")
+let tasmanianDevil = new Mammals ("Tasmanian Devil", "5 years", "mammals", "A predator, then eat meat from other animals such as wallabies and wombats", "The Tasmanian devil (Sarcophilus harrisii) is a carnivorous marsupial of the family Dasyuridae. Until recently, it was only found on the island state of Tasmania, but it has been reintroduced to New South Wales in mainland Australia, with a small breeding population. The size of a small dog, the Tasmanian devil became the largest carnivorous marsupial in the world, following the extinction of the thylacine in 1936. It is related to quolls, and distantly related to the thylacine. It is characterised by its stocky and muscular build, black fur, pungent odour, extremely loud and disturbing screech, keen sense of smell, and ferocity when feeding. The Tasmanian devil's large head and neck allow it to generate among the strongest bites per unit body mass of any extant predatory land mammal. It hunts prey and scavenges on carrion.", "70 cm", "10kg", "Tasmania")
 let quokka = new Mammals ("Quokka", "10 years", "mammals", "Plant eaters, they munch on shrubs and grasses", "The Quokka, Setonix brachyurus, was described by early Dutch explorer, Willem de Vlamingh, 'as a kind of rat as big as a common cat'. His first sighting of the Quokka was on an island off the mouth of the Swan River. He named the island Rottenest ('rat nest') in honour of this sighting. The island is now known as Rottnest Island. Essentially the Quokka looks very much like other wallabies.It has short, very coarse and thick grey-brown fur over most of the body with lighter parts underneath. Its facial features consist of a naked nose on a short, broad face with rounded furry ears.The tail is relatively short and mostly devoid of hair. In contrast, the hair on the feet extends to cover its claws.", "50 cm", "3kg", "Only found on Rottnest Island and a few places on mainland Western Australia.")
 
 let mammalArray = [echidna, tasmanianDevil, quokka]
 
 let sideMammals = document.querySelector(".sidebar")
-let orginalMammal = sideMammals.innerHTML
-let mammal = ""
+let mammalContent = document.querySelector(".welcome_container")
+let originalMammal = mammalContent.innerHTML
+let selectedMammal = "";
 
-const textLength = description => description.slice (0,200) + (description.length > 200 ? "..." : "")
+const textLength = description =>
+    description.substring(0, 200) + (description.length > 200 ? "..." : "");
 
-mammalArray.forEach(animal => {
+mammalArray.forEach(mammal => {
     let mammalsGroup = document.createElement("div");
     mammalsGroup.classList.add("sidebar_item");
-    mammalsGroup.textContent = animal.name;
+    mammalsGroup.textContent = mammal.name;
     sideMammals.appendChild(mammalsGroup);
 
-    // Click event inside loop
+
     mammalsGroup.addEventListener("click", () => {
-        document.querySelectorAll(".sidebar_item").forEach(el => {
-            el.classList.remove("active_item");
+        document.querySelectorAll(".sidebar_item").forEach(element => {
+            element.classList.remove("active_item");
         });
+        if (selectedMammal === mammal.name) {
+            mammalContent.innerHTML = originalMammal;
+            selectedMammal = "";
+        } else {
         mammalsGroup.classList.add("active_item");
-        document.querySelector(".content").innerHTML = `
-            <h1>${animal.name}</h1>
-            <p class="container_text more">Lifespan: ${animal.lifespan}</p>
-            <p class="container_text less">Group: ${animal.group}</p>
-            <p class="container_text less">Food: ${animal.food}</p>
-            <p class="container_text less">Description: ${textLength(animal.description)}</p>
-            <p class="container_text more">Description: ${animal.description}</p>
-            <p class="container_text more">Length: ${animal.length}</p>
-            <p class="container_text more">Weight: ${animal.weight}</p>
-            <p class="container_text more">Found: ${animal.found}</p>`;
-    });
+        document.querySelector(".welcome_container").innerHTML = `
+            <h1>${mammal.name}</h1>
+            <p class="container_text">roup: ${mammal.group}/p>
+            <p class="container_text">Food: ${mammal.food}</p>
+            <p class="container_text less">Description: ${textLength(mammal.description)}</p>
+            <p class="container_text more">Description: ${mammal.description}</p>
+            <p class="container_text more">Length: ${mammal.length}</p>
+            <p class="container_text more">Weight: ${mammal.weight}</p>
+            <p class="container_text more">Found: ${mammal.found}</p>
+            <p class="container_text more">Lifespan: ${mammal.lifespan}</p>
+            <span class="toggle">Read more..</span>`;
+          selectedMammal = mammal.name;
+        }
+        
+          let more = document.querySelectorAll(".more")
+          let less = document.querySelectorAll(".less")
+
+          more.forEach(item => item.style.display = "none")
+          less.forEach(item => item.style.display = "block")
+
+          let toggle = document.querySelector(".toggle")
+
+    toggle.addEventListener("click", () => {
+      if (more[0].style.display === "none") { //added index 0 because we are only checking if one element is hidden or not
+        more.forEach(text => text.style.display = "block");
+        less.forEach(text => text.style.display = "none");
+        toggle.textContent = "Show less..";
+      } else {
+        more.forEach(text => text.style.display = "none");
+        less.forEach(text => text.style.display = "block");
+        toggle.textContent = "Show more..";
+      }
+    })
+  })
 });
