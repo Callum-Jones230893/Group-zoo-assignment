bc8934c8cde061ab413cec654ea5256983e61551
Nathalie <nathaliebuss21@gmail.com>
2025-11-19 11:54:01 +0100

    adding images to constructor as well as the individual animals

--- scripts/mammals.js
+++ scripts/mammals.js
@@ -1,4 +1,5 @@
-function Mammals(name, lifespan, group, food, description, length, weight, found) { 
+//CONSTRUCTOR
+function Mammals(name, lifespan, group, food, description, length, weight, found, image) { 
     this.name = name,
     this.lifespan = lifespan,
     this.group = group,
@@ -6,12 +7,13 @@ function Mammals(name, lifespan, group, food, description, length, weight, found
     this.description = description,
     this.length = length,
     this.weight = weight,
-    this.found = found
+    this.found = found,
+    this.image = image
 }
 
-let echidna = new Mammals ("Echidna", "50 years", "mammals", "insects such as ants and termites, beetle larvae and worms", "Echidnas, also called spiny anteaters, are walking contradictions. They are mammals, but they lay eggs. They are often classified as long- or sort-beaked, but don't have beaks at all, in the traditional sense; they have fleshy noses that can be either on the long side or rather short. They don't really look like true anteaters (Myrmecophaga tridactyla), either, and they are not closely related to them. They are spiny, though; their bodies are covered with hollow, barbless quills. Echidnas are monotremes, egg-laying mammals. The only other living monotreme is the platypus.", "76 cm", "10kg", "Throughout Australia.")
-let tasmanianDevil = new Mammals ("Tasmanian Devil", "5 years", "mammals", "A predator, then eat meat from other animals such as wallabies and wombats", "The Tasmanian devil (Sarcophilus harrisii) is a carnivorous marsupial of the family Dasyuridae. Until recently, it was only found on the island state of Tasmania, but it has been reintroduced to New South Wales in mainland Australia, with a small breeding population. The size of a small dog, the Tasmanian devil became the largest carnivorous marsupial in the world, following the extinction of the thylacine in 1936. It is related to quolls, and distantly related to the thylacine. It is characterised by its stocky and muscular build, black fur, pungent odour, extremely loud and disturbing screech, keen sense of smell, and ferocity when feeding. The Tasmanian devil's large head and neck allow it to generate among the strongest bites per unit body mass of any extant predatory land mammal. It hunts prey and scavenges on carrion.", "70 cm", "10kg", "Tasmania")
-let quokka = new Mammals ("Quokka", "10 years", "mammals", "Plant eaters, they munch on shrubs and grasses", "The Quokka, Setonix brachyurus, was described by early Dutch explorer, Willem de Vlamingh, 'as a kind of rat as big as a common cat'. His first sighting of the Quokka was on an island off the mouth of the Swan River. He named the island Rottenest ('rat nest') in honour of this sighting. The island is now known as Rottnest Island. Essentially the Quokka looks very much like other wallabies.It has short, very coarse and thick grey-brown fur over most of the body with lighter parts underneath. Its facial features consist of a naked nose on a short, broad face with rounded furry ears.The tail is relatively short and mostly devoid of hair. In contrast, the hair on the feet extends to cover its claws.", "50 cm", "3kg", "Only found on Rottnest Island and a few places on mainland Western Australia.")
+let echidna = new Mammals ("Echidna", "50 years", "Mammals", "Insects such as ants and termites, beetle larvae and worms", "Echidnas, also called spiny anteaters, are walking contradictions. They are mammals, but they lay eggs. They are often classified as long- or sort-beaked, but don't have beaks at all, in the traditional sense; they have fleshy noses that can be either on the long side or rather short. They don't really look like true anteaters (Myrmecophaga tridactyla), either, and they are not closely related to them. They are spiny, though; their bodies are covered with hollow, barbless quills. Echidnas are monotremes, egg-laying mammals. The only other living monotreme is the platypus.", "76 cm", "10kg", "Throughout Australia.", "./images/mammals/echidna.jpg")
+let tasmanianDevil = new Mammals ("Tasmanian Devil", "5 years", "Mammals", "A predator, then eat meat from other animals such as wallabies and wombats", "The Tasmanian devil (Sarcophilus harrisii) is a carnivorous marsupial of the family Dasyuridae. Until recently, it was only found on the island state of Tasmania, but it has been reintroduced to New South Wales in mainland Australia, with a small breeding population. The size of a small dog, the Tasmanian devil became the largest carnivorous marsupial in the world, following the extinction of the thylacine in 1936. It is related to quolls, and distantly related to the thylacine. It is characterised by its stocky and muscular build, black fur, pungent odour, extremely loud and disturbing screech, keen sense of smell, and ferocity when feeding. The Tasmanian devil's large head and neck allow it to generate among the strongest bites per unit body mass of any extant predatory land mammal. It hunts prey and scavenges on carrion.", "70 cm", "10kg", "Tasmania", "./images/mammals/tasmania-devil.jpg")
+let quokka = new Mammals ("Quokka", "10 years", "Mammals", "Plant eaters, they munch on shrubs and grasses", "The Quokka, Setonix brachyurus, was described by early Dutch explorer, Willem de Vlamingh, 'as a kind of rat as big as a common cat'. His first sighting of the Quokka was on an island off the mouth of the Swan River. He named the island Rottenest ('rat nest') in honour of this sighting. The island is now known as Rottnest Island. Essentially the Quokka looks very much like other wallabies.It has short, very coarse and thick grey-brown fur over most of the body with lighter parts underneath. Its facial features consist of a naked nose on a short, broad face with rounded furry ears.The tail is relatively short and mostly devoid of hair. In contrast, the hair on the feet extends to cover its claws.", "50 cm", "3kg", "Only found on Rottnest Island and a few places on mainland Western Australia.", "./images/mammals/quokka.jpg")
 
 let mammalArray = [echidna, tasmanianDevil, quokka]
 
@@ -41,7 +43,7 @@ mammalArray.forEach(mammal => {
         mammalsGroup.classList.add("active_item");
         document.querySelector(".welcome_container").innerHTML = `
             <h1>${mammal.name}</h1>
-            <p class="container_text">roup: ${mammal.group}/p>
+            <p class="container_text">Group: ${mammal.group}</p>
             <p class="container_text">Food: ${mammal.food}</p>
             <p class="container_text less">Description: ${textLength(mammal.description)}</p>
             <p class="container_text more">Description: ${mammal.description}</p>
@@ -49,7 +51,8 @@ mammalArray.forEach(mammal => {
             <p class="container_text more">Weight: ${mammal.weight}</p>
             <p class="container_text more">Found: ${mammal.found}</p>
             <p class="container_text more">Lifespan: ${mammal.lifespan}</p>
-            <span class="toggle">Read more..</span>`;
+            <span class="toggle">Read more..</span>
+            <img class="container_text" src="${mammal.image}"></img>`;
           selectedMammal = mammal.name;
         }
         
